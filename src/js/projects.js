e12a38d2eb645138bb760fa3e04033aae8c48606
Angelina <midnightshout03@gmail.com>
2024-03-15 20:11:07 +0200

    js

---
+++ src/js/projects.js
@@ -0,0 +1,22 @@
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+// slidesPerView: '1',
+//   spaceBetween: 0,
+//   direction: 'horizontal',
+// //   loop: true,
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+
+

9581f1cbf59e122e9f18bf88edf82e0b8cae4fa5
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-03-16 15:19:19 -0400

    UPDATE

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -44,29 +44,34 @@ function renderSlides(reviewsData) {
   try {
     const reviewsData = await fetchData();
     renderSlides(reviewsData);
-    swiper.update();
+    initSwiper();
   } catch (error) {
     console.error('Error rendering reviews:', error);
   }
 });
 
-const swiper = new Swiper(".swiper", {
-    spaceBetween: 100,
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
     autoplay: {
       delay: 2000,
       stopOnLastSlide: true,
       disableOnInteraction: false
-      },
+    },
     breakpoints:{
-        375: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        },
-        1440:{
-            slidesPerView: 4
-        }
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16
+      },
+      1440:{
+        slidesPerView: 4,
+        spaceBetween: 17
+      }
     },
     speed: 700,
     navigation: {
@@ -79,23 +84,12 @@ const swiper = new Swiper(".swiper", {
       clickable: true,
     },
     keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
     },
-    on: {
-        reachEnd: function() {
-            document.querySelector('.swiper-button-next').disabled = true;
-            document.querySelector('.swiper-button-prev').disabled = true;
-        },
-        reachBeginning: function() {
-
-            document.querySelector('.swiper-button-next').disabled = false;
-            document.querySelector('.swiper-button-prev').disabled = false;
-        }
-    }
   });
-  
+
   const sliderNext = document.querySelector(".swiper-button-next");
   const sliderPrev = document.querySelector(".swiper-button-prev");
 
@@ -106,73 +100,4 @@ const swiper = new Swiper(".swiper", {
   sliderPrev.addEventListener('click', () => {
     swiper.slidePrev(500);
   });
-
-
-
-
-// function initSwiper() {
-//   let slidesPerView = 1;
-//   if (window.innerWidth >= 768 && window.innerWidth < 1440) {
-//     slidesPerView = 2;
-//   } else if (window.innerWidth >= 1440) {
-//     slidesPerView = 4;
-//   }
-//   const swiper = new Swiper(".swiper", {
-//     loop: true,
-//     spaceBetween: 100,
-//     slidesPerView: slidesPerView,
-//     autoplay: {
-//       delay: 2000,
-//       stopOnLastSlide: true,
-//       disableOnInteraction: false
-//       },
-//     breakpoints:{
-//         375: {
-//             slidesPerView: 1
-//         },
-//         768: {
-//             slidesPerView: 2
-//         },
-//         1440:{
-//             slidesPerView: 4
-//         }
-//     },
-//     speed: 700,
-//     navigation: {
-//       nextEl: '.swiper-button-next', 
-//       prevEl: '.swiper-button-prev',
-//       clickable: true,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     keyboard: {
-//         enabled: true,
-//         onlyInViewport: true,
-//         pageUpDown: true,
-//     },
-//     on: {
-//         reachEnd: function() {
-//             document.querySelector('.swiper-button-next').disabled = true;
-//             document.querySelector('.swiper-button-prev').disabled = true;
-//         },
-//         reachBeginning: function() {
-
-//             document.querySelector('.swiper-button-next').disabled = false;
-//             document.querySelector('.swiper-button-prev').disabled = false;
-//         }
-//     }
-//   });
-  
-//   const sliderNext = document.querySelector(".swiper-button-next");
-//   const sliderPrev = document.querySelector(".swiper-button-prev");
-
-//   sliderNext.addEventListener('click', () => {
-//     swiper.slideNext(500);
-//   });
-
-//   sliderPrev.addEventListener('click', () => {
-//     swiper.slidePrev(500);
-//   });
-// }
+}

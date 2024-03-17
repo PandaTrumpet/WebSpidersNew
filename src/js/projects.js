94c62bcc1893e81dfe8707a6d70c5e30d6b79768
Angelina <midnightshout03@gmail.com>
2024-03-17 10:54:52 +0200

    swiper+styles+html

--- src/js/projects.js
+++ src/js/projects.js
@@ -5,23 +5,56 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const sliderNex = document.querySelector('.swiper-button-next')
- sliderNex.addEventListener('click',()=>{ 
-    console.log('Hello');
-     swiper.slideNext(500)}
+// const sliderNex = document.querySelector('.swiper-button-next')
+//  sliderNex.addEventListener('click',()=>{ 
+//     console.log('Hello');
+//      swiper.slideNext(500)}
     
     
- )
- const sliderprev = document.querySelector('.swiper-button-prev')
-sliderprev.addEventListener('click', () => {
-    swiper.slidePrev(500)
- })
- const swiper = new Swiper('.swiper', {
-     speed: 400,
-     spaceBetween: 100,
-     direction: 'horizontal',
- });
+//  )
+//  const sliderprev = document.querySelector('.swiper-button-prev')
+// sliderprev.addEventListener('click', () => {
+//     swiper.slidePrev(500)
+//  })
+//  const swiper = new Swiper('.swiper', {
+//      speed: 400,
+//      spaceBetween: 100,
+//      direction: 'horizontal',
+//  });
 
-   const swipers = document.querySelector('.swiper').swiper;
-swiper.slidePrev()
+//    const swipers = document.querySelector('.swiper').swiper;
+// swiper.slidePrev()
  
+  const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    enabled: true,
+    sensitivity: 1,
+    eventsTarget: '.swiper',
+  },
+  navigation: {
+      nextEl: '.swiper-button-next', 
+      prevEl: '.swiper-button-prev',
+      clickable: true,
+    },
+  effect: 'slide',
+  slideToClickedSlide: true,
+  touchRatio: 3,
+  allowTouchMove: true,
+});
+
+  const sliderNext = document.querySelector(".swiper-button-next");
+  const sliderPrev = document.querySelector(".swiper-button-prev");
+
+  sliderNext.addEventListener('click', () => {
+    swiper.slideNext(500);
+  });
+
+  sliderPrev.addEventListener('click', () => {
+    swiper.slidePrev(500);
+  });

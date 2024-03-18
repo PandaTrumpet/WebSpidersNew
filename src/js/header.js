32bfaf26f45c077dd3e7fc1cdc1057d4a74dc3f8
PandaTrumpet <dsavontrumpet@gmail.com>
2024-03-18 14:58:46 +0200

    update header

--- src/js/header.js
+++ src/js/header.js
@@ -1,35 +1,35 @@
-const menuBtn = document.querySelector(".menu-button");
-const list = document.querySelector(".menu-list")
-const mobileBtn = document.querySelector(".burger-btn");
-const closeBtn = document.querySelector(".mobile-close-btn");
-const mobileMenu = document.querySelector(".mobile-menu")
-const mobileOrderBtn = document.querySelector(".mobile-order-btn")
-const workSection = document.getElementById('footer-work-together')
-const orderBtn = document.querySelector('.order-btn')
-const mobileList = document.querySelector('.mobile-menu-list')
-const item = document.querySelector(".menu-link")
+const menuBtn = document.querySelector('.menu-button');
+const list = document.querySelector('.menu-list');
+const mobileBtn = document.querySelector('.burger-btn');
+const closeBtn = document.querySelector('.mobile-close-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileOrderBtn = document.querySelector('.mobile-order-btn');
+const workSection = document.getElementById('footer-work-together');
+const orderBtn = document.querySelector('.order-btn');
+const mobileList = document.querySelector('.mobile-menu-list');
+const item = document.querySelector('.menu-link');
 
 mobileBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('visually-hidden');
+  mobileMenu.classList.toggle('visually-hidden');
 });
 closeBtn.addEventListener('click', () => {
-    mobileMenu.classList.add('visually-hidden');
+  mobileMenu.classList.add('visually-hidden');
 });
 mobileOrderBtn.addEventListener('click', () => {
-    window.location.href = "#footer-work-together";
-    if (mobileOrderBtn) {
-        mobileMenu.classList.add('visually-hidden');
-    }
+  window.location.href = '#footer-work-together';
+  if (mobileOrderBtn) {
+    mobileMenu.classList.add('visually-hidden');
+  }
 });
 orderBtn.addEventListener('click', () => {
-    window.location.href = "#footer-work-together"
-})
-menuBtn.addEventListener('click', (event) => {
-    list.classList.toggle('active');
+  window.location.href = '#footer-work-together';
+});
+menuBtn.addEventListener('click', event => {
+  list.classList.toggle('active');
 });
 
 mobileList.addEventListener('click', () => {
-    if (mobileList) {
-        mobileMenu.classList.add('visually-hidden');
-    }
-})
+  if (mobileList) {
+    mobileMenu.classList.add('visually-hidden');
+  }
+});

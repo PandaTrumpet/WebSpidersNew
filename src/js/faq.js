9c1f9d493deabe94d0ecaf9658dfbe223c65ee21
VitaPad <v.mars22@ukr.net>
2024-03-18 16:23:31 +0200

    editing styles

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,10 +1,9 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-new Accordion('.accordion-container', {
+new Accordion('.faq-container .accordion-container', {
   duration: 400,
   showMultiple: true,
 });
-
 document.addEventListener('DOMContentLoaded', function () {
   const accordionContainer = document.querySelector('.accordion-container');
 

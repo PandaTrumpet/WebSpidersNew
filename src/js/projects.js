6e2dfab7c2a2789cd725a6f63405d685630a57a4
Angelina <midnightshout03@gmail.com>
2024-03-19 09:05:10 +0200

    update js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,31 +1,4 @@
-import 'swiper/css'
-import Swiper from 'swiper';
-
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// const sliderNex = document.querySelector('.swiper-button-next')
-//  sliderNex.addEventListener('click',()=>{ 
-//     console.log('Hello');
-//      swiper.slideNext(500)}
-    
-    
-//  )
-//  const sliderprev = document.querySelector('.swiper-button-prev')
-// sliderprev.addEventListener('click', () => {
-//     swiper.slidePrev(500)
-//  })
-//  const swiper = new Swiper('.swiper', {
-//      speed: 400,
-//      spaceBetween: 100,
-//      direction: 'horizontal',
-//  });
-
-//    const swipers = document.querySelector('.swiper').swiper;
-// swiper.slidePrev()
- 
-  const swiperProject = new Swiper('.main-swiper-container', {
+const swiperProject = new Swiper('.main-swiper-container', {
   direction: 'horizontal',
   keyboard: {
     enabled: true,
@@ -35,73 +8,38 @@ import 'swiper/css/pagination';
   mousewheel: {
     enabled: true,
     sensitivity: 1,
-    eventsTarget: '.swiper',
+    eventsTarget: '.main-swiper-container',
   },
   navigation: {
-      nextEl: '.btn-n', 
-      prevEl: '.btn-p',
-      clickable: true,
-    },
+    nextEl: '.btn-n',
+    prevEl: '.btn-p',
+    clickable: true,
+  },
   effect: 'slide',
   slideToClickedSlide: true,
   touchRatio: 3,
   allowTouchMove: true,
 });
 
-const sliderNext = document.querySelector(".btn-n");
-const sliderPrev = document.querySelector(".btn-p");
-  
-//   function updateNavButtons() {
-//     if (swiper.isBeginning) {
-//         sliderPrev.disabled = true;
-//     } else {
-//         sliderPrev.disabled = false;
-//     }
-
-//     if (swiper.isEnd) {
-//         sliderNext.disabled = true;
-//     } else {
-//         sliderNext.disabled = false;
-//     }
-// }
+const sliderNext = document.querySelector('.btn-n');
+const sliderPrev = document.querySelector('.btn-p');
 
 function updateNavButtons() {
-    if (swiperProject.isBeginning) {
-        sliderPrev.classList.add('swiper-button-disabled'); 
-        sliderPrev.disabled = true;
-    } else {
-        sliderPrev.classList.remove('swiper-button-disabled');
-        sliderPrev.disabled = false;
-    }
+  if (swiperProject.isBeginning) {
+    sliderPrev.classList.add('swiper-button-disabled');
+    sliderPrev.disabled = true;
+  } else {
+    sliderPrev.classList.remove('swiper-button-disabled');
+    sliderPrev.disabled = false;
+  }
 
-    if (swiperProject.isEnd) {
-        sliderNext.classList.add('swiper-button-disabled'); 
-        sliderNext.disabled = true;
-    } else {
-        sliderNext.classList.remove('swiper-button-disabled'); 
-        sliderNext.disabled = false;
-    }
+  if (swiperProject.isEnd) {
+    sliderNext.classList.add('swiper-button-disabled');
+    sliderNext.disabled = true;
+  } else {
+    sliderNext.classList.remove('swiper-button-disabled');
+    sliderNext.disabled = false;
+  }
 }
 
-swiperProject.on('slideChange', updateNavButtons);
-
-  sliderNext.addEventListener('click', () => {
-    swiperProject.slideNext(500);
-  });
-
-  sliderPrev.addEventListener('click', () => {
-    swiperProject.slidePrev(500);
-  });
-
-document.addEventListener('keydown', (event) => {
-    if (event.key === "ArrowRight" || event.key === "ArrowUp")
-        swiperProject.slideNext(500);
-});
-  
-document.addEventListener('keydown', (event) => {
-    if (event.key === "ArrowLeft" || event.key === "ArrowDown")
-        swiperProject.slidePrev(500);
-});
-
 updateNavButtons();
-
